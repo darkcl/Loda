7337bc0d76ae8a817201cbfbd475d08e7823cd2a
Yeung Yiu Hung <hkclex@gmail.com>
2019-10-29 15:13:22 +0800

    feat(downloader): add url downloader params

--- lib/downloader/url_downloader.go
+++ lib/downloader/url_downloader.go
@@ -3,11 +3,37 @@ package downloader
 // URLDownloader download file from url
 type URLDownloader struct {
 	Downloader
+	URL              string
+	Destination      string
+	NumOfConnections int
+	IsResumable      bool
+}
+
+// URLDownloaderParams params for setting up a URL Downloader
+type URLDownloaderParams struct {
+	URL              string
+	Destination      string
+	NumOfConnections int
+	IsResumable      bool
+}
+
+// NewURLDownloader creates an url downloader
+func NewURLDownloader(parms URLDownloaderParams) *URLDownloader {
+	return &URLDownloader{
+		URL:              parms.URL,
+		Destination:      parms.Destination,
+		NumOfConnections: parms.NumOfConnections,
+		IsResumable:      parms.IsResumable,
+	}
 }
 
 // PreProcess pre process a url, e.g. getting if this url support resume download
 func (u URLDownloader) PreProcess() {
+	// Get URL headers
 
+	// Get Destination
+
+	// Get Resumable Meta data
 }
 
 // Process will start a download process
@@ -17,7 +43,7 @@ func (u URLDownloader) Process() {
 
 // PostProcess will clean up files
 func (u URLDownloader) PostProcess() {
-
+	// Remove meta data
 }
 
 // Done specify this task is done
