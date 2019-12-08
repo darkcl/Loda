e0bc7715f0a0cb9b33c4eeea6d17af7696cd94a9
Yeung Yiu Hung <446404+darkcl@users.noreply.github.com>
2019-12-08 13:43:26 +0800

    feat(ui): add task item component

--- ui/src/views/DownloadList.tsx
+++ ui/src/views/DownloadList.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 
 import { Button, Pane, Heading } from "evergreen-ui";
+import { DownloadTaskItem } from "../compoents";
 import { ModalStore, ModalActions } from "../store";
 
 export const DownloadList: React.FunctionComponent = () => {
   const modalDispatch = React.useContext(ModalStore.Dispatch);
 
   return (
-    <>
+    <Pane flexDirection="column">
       <Pane display="flex" padding={16} borderRadius={3}>
         <Pane flex={1} alignItems="center" display="flex">
           <Heading size={600}>Tasks</Heading>
@@ -35,6 +36,10 @@ export const DownloadList: React.FunctionComponent = () => {
           </Button>
         </Pane>
       </Pane>
-    </>
+
+      <Pane flex={1} alignItems="center" display="flex">
+        <DownloadTaskItem></DownloadTaskItem>
+      </Pane>
+    </Pane>
   );
 };
